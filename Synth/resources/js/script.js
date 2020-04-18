0d40f323ec8718489a585e4e9dd1c8e545962322
bocacrst <61462231+bocacrst@users.noreply.github.com>
2020-04-18 20:00:52 +0300

    jgvqdf

--- Synth/resources/js/script.js
+++ Synth/resources/js/script.js
@@ -38,6 +38,123 @@ $(document).ready(function(){
      });
 });
 
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+
+const nextBtn = document.querySelector('.carousel__button--right');
+const prevBtn = document.querySelector('.carousel__button--left');
+const dotNav = document.querySelector('.carousel__nav');
+const dots = Array.from(dotNav.children);
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+const firstSlide = track.querySelector('.current-slide');
+const firstDot = dotNav.querySelector('.current-slide');
+const lastSlide = track.querySelector('.last-slide');
+const lastDot = dotNav.querySelector('.last-button');
+
+var x=0;
+
+const setSlidePossition = (slide,index)=>{
+    
+    slide.style.left = slideWidth * index + 'px';
+}
+
+slides.forEach(setSlidePossition);
+
+
+const moveToSlide = (track,currentSlide,targetSlide)=>{
+    track.style.transform = `translateX(-${targetSlide.style.left})`;
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+const updateDots = (currentDot,targetDot)=>{
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
+
+nextBtn.addEventListener('click', e =>{
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    x = 1;   
+    if(!nextSlide){
+    
+        moveToSlide(track,currentSlide,firstSlide);
+        updateDots(currentDot, firstDot);
+
+    }else{
+        
+        moveToSlide(track,currentSlide,nextSlide);
+        updateDots(currentDot, nextDot);
+        
+    }
+})
+
+prevBtn.addEventListener('click', e =>{
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotNav.querySelector('.current-slide');
+    const prevDot = currentDot.previousElementSibling;
+    x = 1;
+
+    if(!prevSlide){
+      
+        moveToSlide(track,currentSlide,lastSlide);
+        updateDots(currentDot, lastDot);
+
+    }else{
+      
+        moveToSlide(track,currentSlide,prevSlide);
+        updateDots(currentDot, prevDot);
+    }
+})
+
+dotNav.addEventListener('click', e =>{
+    const targetDot = e.target.closest('button');
+
+    if(!targetDot)
+    return
+
+    const currentDot = dotNav.querySelector('.current-slide');
+    const currentSlide = track.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track,currentSlide,targetSlide);
+    updateDots(currentDot,targetDot);
+    
+})
+
+//-----TIMER----//
+
+const autoChange = ()=>{
+
+    if(x!=0)
+    return
+
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+  
+    if(!nextSlide){
+    
+        moveToSlide(track,currentSlide,firstSlide);
+        updateDots(currentDot, firstDot);
+
+    }else{
+        
+        moveToSlide(track,currentSlide,nextSlide);
+        updateDots(currentDot, nextDot);   
+    }
+
+        setTimeout(autoChange,3500);
+}
+
+window.onload = autoChange;
+
+
 
 
 
